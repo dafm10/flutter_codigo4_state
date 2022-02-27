4496299eda665af691c57fafd7f08a558ea8fa4d
dafm10 <dafm.10@gmail.com>
2022-02-27 18:27:03 -0500

    Singleton 2

--- lib/services/superheroe_service.dart
+++ lib/services/superheroe_service.dart
@@ -3,13 +3,28 @@ import 'dart:async';
 
 import 'package:flutter_codigo4_state/models/superhero_model.dart';
 
-class SuperHeroeService{
+class _SuperheroeService{
+
+  /*// Aplicamos Singleton modo 1
+  SuperheroeService._();
+
+  static final SuperheroeService _instance = SuperheroeService._();
+
+  factory SuperheroeService(){
+    return _instance;
+  }
+  // fin singleton modo 1*/
+
   Superheroe? _superheroe;
 
   StreamController<Superheroe> _superheroeStream = StreamController();
 
+  Stream<Superheroe> get superheroeStream => _superheroeStream.stream;
+
+  Superheroe get superheroe => _superheroe!;
+
   createSuperheroe(Superheroe superheroe){
-    _superheroe = _superheroe;
+    _superheroe = superheroe;
     _superheroeStream.add(_superheroe!);
   }
 
@@ -17,3 +32,6 @@ class SuperHeroeService{
     _superheroeStream.close();
   }
 }
+
+// modo 2 de crear singleton, primero ponemos la clase en privado
+final superheroeService = _SuperheroeService();
