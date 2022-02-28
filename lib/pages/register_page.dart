254bc0233dcb6edd5eefceddcec0c1bee68527c6
dafm10 <dafm.10@gmail.com>
2022-02-27 20:17:19 -0500

    Provider 3

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_codigo4_state/models/superhero_model.dart';
+import 'package:flutter_codigo4_state/providers/superheroe_provider.dart';
+import 'package:provider/provider.dart';
 
 class RegisterPage extends StatelessWidget {
   const RegisterPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    // Aqui estamos a la escucha de lo que ocurre
+    final SuperheroProvider _superheroProvider =
+        Provider.of<SuperheroProvider>(context);
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.deepPurpleAccent,
@@ -15,8 +22,20 @@ class RegisterPage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             MaterialButton(
-              onPressed: () {},
-              child: Text(
+              onPressed: () {
+                Superheroe superman = Superheroe(
+                  name: "Superman",
+                  experience: 3,
+                  powers: [
+                    "Super fuerza",
+                    "Visión láser",
+                    "Volar",
+                    "Visión X"
+                  ],
+                );
+                _superheroProvider.createSuperhero(superman);
+              },
+              child: const Text(
                 "Añadir superheroe",
                 style: TextStyle(
                   color: Colors.white,
