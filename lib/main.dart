e4509a97ba18888ef47a734c6933ae6c64293661
dafm10 <dafm.10@gmail.com>
2022-02-27 20:08:18 -0500

    Provider 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_codigo4_state/pages/home_page.dart';
+import 'package:flutter_codigo4_state/providers/superheroe_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (BuildContext context) => SuperheroeProvider(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const HomePage(),
       ),
-      home: const HomePage(),
     );
   }
 }
