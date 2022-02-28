baa2c7e969ecca04489c65302890b4e476b618df
dafm10 <dafm.10@gmail.com>
2022-02-27 21:29:01 -0500

    Cubit 2

---
+++ lib/cubit/superheroe/superheroe_cubit.dart
@@ -0,0 +1,21 @@
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SuperheroeCubit extends Cubit {
+
+  SuperheroeCubit(initialState) : super(initialState);
+
+  // Implementar nuestros métodos
+
+
+  // Crear superheroe
+
+  // Actualizar superheroe
+
+
+  // Eliminar superheroe
+
+
+  // Agregar poderes
+
+}
