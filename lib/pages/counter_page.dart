6cff4fddf8d8e3cb6c788d2b69a180f3606499d7
dafm10 <dafm.10@gmail.com>
2022-02-27 19:48:58 -0500

    Counter 3

--- lib/pages/counter_page.dart
+++ lib/pages/counter_page.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_codigo4_state/services/counter_service.dart';
 
-class CounterPage extends StatelessWidget {
+class CounterPage extends StatefulWidget {
   const CounterPage({Key? key}) : super(key: key);
+
+  @override
+  State<CounterPage> createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    counterService.closeStream();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,8 +37,9 @@ class CounterPage extends StatelessWidget {
               stream: counterService.counterStream,
               builder: (BuildContext context, AsyncSnapshot snap){
                 if(snap.hasData){
+                  int counter = snap.data;
                   return Text(
-                    snap.data.toString(),
+                    counter.toString(),
                     style: const TextStyle(
                       fontSize: 40.0,
                       color: Colors.black54,
